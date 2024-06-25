e8374d052c1d982b9930265bcc3c3dc34b2c7634
Kuba Jarmołowicz <k.jarmolowicz@bsgroup.eu>
2024-06-25 17:41:21 +0200

    basic task done

--- routes.js
+++ routes.js
@@ -3,33 +3,44 @@ const { ROUTES, METHODS } = require("./consts");
 
 const requestHandler = (req, res) => {
   const url = req.url;
+  const method = req.method;
 
   switch (url) {
     case ROUTES.USERS: {
-      const dummyUsers = []
-        .fill("User", 10)
+      const dummyUsers = new Array(10)
+        .fill("User")
         .map((user, index) => `${user} ${index + 1}`);
-      const dummyList = `<ul>${dummyUsers.map(
-        (dummyUser) => `<li>${dummyUser}</li>`
+      const dummyList = `<ul>${dummyUsers.reduce(
+        (acc, dummyUser) => (acc += `<li>${dummyUser}</li>`),
+        ""
       )}</ul>`;
 
       return templates.basicPage(res, dummyList);
     }
     case ROUTES.CREATE_USER: {
-      const chunks = [];
+      if (method !== METHODS.POST) {
+        res.statusCode = 302;
+        res.setHeader("Location", ROUTES.HOME);
+        return res.end();
+      }
+
+      const body = [];
       req.on("data", (chunk) => {
-        chunks.push(chunk);
+        body.push(chunk);
       });
 
       req.on("end", () => {
-        const parsedData = Buffer(chunks).toString();
+        const parsedData = Buffer.concat(body).toString().split("=")[1];
         console.log("###", { parsedData });
-        res.end();
+        res.statusCode = 302;
+        res.setHeader("Location", ROUTES.HOME);
+        return res.end();
       });
+      break;
     }
     default: {
       const greeting = "<h1>Welcome to Node.js practice app!</h1>";
-      const form = `<form><input method='${METHODS.POST}' action='/create-user' type='text' name='username'/><button type='submit'>Send</button><form/>`;
+      const form = `<form method='${METHODS.POST}' action='${ROUTES.CREATE_USER}'><input type='text' name='username'/><button type='submit'>Send</button><form/>`;
       return templates.basicPage(res, `<div>${greeting}${form}</div>`);
     }
   }
