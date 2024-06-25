0a3a278fb0478e5e4e8ab9aca40e84ab7237f870
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2024-06-25 20:03:49 +0200

    task 2 complete

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require("http");
+const express = require("express");
 
-const { handler } = require("./routes");
+const app = express();
 
-const server = http.createServer(handler);
+app.use("/users", (req, res, next) => {
+  console.log("I only run for /users!");
+  res.send("<h1>Welcome to '/users' my man!</h1>");
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("I run for home!");
+  res.send("<h1>Pretty h1 for the prettiest website of all!</h1>");
+});
+
+app.listen(3000);
